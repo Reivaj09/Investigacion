24576e614a646426b20d2f71e5645a0bf45cc53e
Javier <javirimo@hotmail.com>
2021-04-11 14:26:34 -0600

    "Arreglé pequeño error con respecto a código inecesario en BigClass.cpp (Eliminé el desarrollo del toString ya que al final decidí quitarlo del .h pero se me olvidó eliminarlo del .cpp)"

---
+++ cmake-build-debug/CMakeFiles/untitled1.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/javir/OneDrive/Escritorio/PrograII/Trabajo de Investigacion/Investigacion/Adaption_Little_Class1.cpp" "C:/Users/javir/OneDrive/Escritorio/PrograII/Trabajo de Investigacion/Investigacion/cmake-build-debug/CMakeFiles/untitled1.dir/Adaption_Little_Class1.cpp.obj"
+  "C:/Users/javir/OneDrive/Escritorio/PrograII/Trabajo de Investigacion/Investigacion/Big_Class.cpp" "C:/Users/javir/OneDrive/Escritorio/PrograII/Trabajo de Investigacion/Investigacion/cmake-build-debug/CMakeFiles/untitled1.dir/Big_Class.cpp.obj"
+  "C:/Users/javir/OneDrive/Escritorio/PrograII/Trabajo de Investigacion/Investigacion/IPaymentProccess.cpp" "C:/Users/javir/OneDrive/Escritorio/PrograII/Trabajo de Investigacion/Investigacion/cmake-build-debug/CMakeFiles/untitled1.dir/IPaymentProccess.cpp.obj"
+  "C:/Users/javir/OneDrive/Escritorio/PrograII/Trabajo de Investigacion/Investigacion/Little_Class1.cpp" "C:/Users/javir/OneDrive/Escritorio/PrograII/Trabajo de Investigacion/Investigacion/cmake-build-debug/CMakeFiles/untitled1.dir/Little_Class1.cpp.obj"
+  "C:/Users/javir/OneDrive/Escritorio/PrograII/Trabajo de Investigacion/Investigacion/main.cpp" "C:/Users/javir/OneDrive/Escritorio/PrograII/Trabajo de Investigacion/Investigacion/cmake-build-debug/CMakeFiles/untitled1.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/untitled1.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled1.dir/Adaption_Little_Class1.cpp.obj"
+  "CMakeFiles/untitled1.dir/Big_Class.cpp.obj"
+  "CMakeFiles/untitled1.dir/IPaymentProccess.cpp.obj"
+  "CMakeFiles/untitled1.dir/Little_Class1.cpp.obj"
+  "CMakeFiles/untitled1.dir/main.cpp.obj"
+  "untitled1.exe"
+  "untitled1.exe.manifest"
+  "untitled1.lib"
+  "untitled1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
